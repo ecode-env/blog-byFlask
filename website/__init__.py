930b37ab9b29aab53e8d342a20b49c44d6798f9d
eyobScript <eyobbmulugeta@gmail.com>
2025-03-11 11:02:16 +0300

    create a function that return the app

--- website/__init__.py
+++ website/__init__.py
@@ -2,3 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from os import path
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "hello this is eyob from flask."
+
+    return app
