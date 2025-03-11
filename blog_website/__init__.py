04abebacf580f8d0c690c3953527138c1ea5a947
eyobScript <eyobbmulugeta@gmail.com>
2025-03-11 22:25:21 +0300

    register blueprint

--- blog_website/__init__.py
+++ blog_website/__init__.py
@@ -18,11 +18,10 @@ def create_app():
     from .views import views
     from .auth import auth
 
-    create_database(app)
-
-
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
+
+    create_database(app)
 
     return app
 
