2ad0e7b91047faf1b44057e26abf2c5f1aae8d68
eyobScript <eyobbmulugeta@gmail.com>
2025-03-11 21:54:53 +0300

    blog

--- blog_website/__init__.py
+++ blog_website/__init__.py
@@ -3,15 +3,30 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from os import path
 
+import app
+
+db = SQLAlchemy()
+DB_NAME = "blog.db"
+
+
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "hello this is eyob from flask."
+    app.config["SQL_ALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
+
+    create_database(app)
 
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
     return app
+
+def create_database(app):
+    if not path.exists("blog_website/" + DB_NAME):
+        db.create_all(app)
+        print('Database create successfully')
