de678ebf8f6b9901f8754c91fcf55bc9cb6e5bc0
eyobScript <eyobbmulugeta@gmail.com>
2025-03-12 16:02:40 +0300

    post table

--- blog_website/models.py
+++ blog_website/models.py
@@ -8,3 +8,9 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String)
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
