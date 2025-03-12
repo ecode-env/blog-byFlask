5c72508215931e3a62fa4e2ff71274e415d9af68
eyobScript <eyobbmulugeta@gmail.com>
2025-03-12 16:10:58 +0300

    make one-to-money relationship

--- blog_website/models.py
+++ blog_website/models.py
@@ -7,10 +7,12 @@ class User(db.Model, UserMixin):
     email =db.Column(db.String(150), unique=True)
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String)
+    post = db.relationship('Post', backref='user', passive_deletes=True)
     date = db.Column(db.DateTime(timezone=True), default=func.now())
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String, nullable=False)
     title = db.Column(db.String, nullable=False)
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     date = db.Column(db.DateTime(timezone=True), default=func.now())
