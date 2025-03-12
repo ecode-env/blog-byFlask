b34b15a8c5d5f829e554d9ac41a4475ff996bf25
eyobScript <eyobbmulugeta@gmail.com>
2025-03-12 10:40:28 +0300

    current user

--- blog_website/auth.py
+++ blog_website/auth.py
@@ -2,7 +2,7 @@ import re
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from .models import User
 from . import db
-from flask_login import login_user, login_required, logout_user
+from flask_login import login_user, login_required, logout_user,current_user
 from werkzeug.security import generate_password_hash,check_password_hash
 
 
