1aba44b28eb6dbdfdaae8661df43d584197e9a7a
eyobScript <eyobbmulugeta@gmail.com>
2025-03-12 16:26:57 +0300

    post logic

--- blog_website/views.py
+++ blog_website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request,flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from .models import Post
 from . import db
@@ -15,4 +15,23 @@ def home():
 @views.route('/create-post', methods=['GET','POST'])
 @login_required
 def create_post():
+    if request.method == 'POST':
+        content = request.form.get('content')
+        title = request.form.get('title')
+
+        if not content:
+            flash(message='Content cannot be empty!', category='error')
+        if not title:
+            flash(message='Title cannot be empty!', category='error')
+        else:
+            post = Post(
+                title=title,
+                text=content,
+                author=current_user.id
+            )
+            db.session.add(post)
+            db.session.commit()
+            flash(message='Post Created', category='success')
+            return redirect(url_for('views.home'))
+
     return render_template('create_post.html', user=current_user)
