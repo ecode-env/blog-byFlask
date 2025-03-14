b7c1a26ccdfe83e9d61d664448ebf175b6f6471c
eyobScript <eyobbmulugeta@gmail.com>
2025-03-14 17:20:30 +0300

    import like

--- blog_website/views.py
+++ blog_website/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 from .decorators import admin_required
-from .models import Post, Comment
+from .models import Post, Comment, Like
 from . import db
 
 views = Blueprint("views", __name__)
